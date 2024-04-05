c591505f85069840d07bb3ef7e68d6ae3f346f0f
Huongcutee <nguyensinhhuong123@gmail.com>
2024-04-05 17:54:09 +0700

    Code gi do khong biet

--- app/[lang]/(protected)/student/profile/page.tsx
+++ app/[lang]/(protected)/student/profile/page.tsx
@@ -1,33 +1,18 @@
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { NavLeft } from "@/components/ui/group-function-user";
 
-
-const StudentProfilePage = ()=> {
+const StudentProfilePage = () => {
   return (
-  <>
-     <div className="group/sidebar h-screen relative w-72 p-3 flex flex-col bg-gray-100 gap-3 rounded-r-3xl dark:bg-[#2a2d30] shadow-2xl scrollbar-hide">
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      
-    </div>
-  </>
+    <>
+      <div className="container flex bg-[#cccccccc] py-8 text-primary">
+        <NavLeft />
+      </div>
+    </>
   );
-}; 
-export default  StudentProfilePage;
+};
+export default StudentProfilePage;
