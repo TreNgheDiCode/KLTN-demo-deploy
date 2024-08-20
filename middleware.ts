fe333b736d9b942134e17496cc03e829283095b0
TreNgheDiCode <gabayan170@gmail.com>
2024-08-20 11:36:59 +0700

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -14,10 +14,10 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   // Analytics
   if (
-    !config.matcher.some((pattern) =>
-      new RegExp(pattern).test(req.nextUrl.pathname),
-    )
+    publicRoutes.includes(req.nextUrl.pathname) ||
+    !req.nextUrl.pathname.startsWith("/api")
   ) {
+    console.log("Tracking pageview", req.nextUrl.pathname);
     analytics.track("pageview", {
       page: req.nextUrl.pathname,
       country: req.geo?.country,
